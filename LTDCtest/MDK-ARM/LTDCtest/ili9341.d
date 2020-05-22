58c80a091c267e5a4b4f0246b7eca44c6ea7eee9
Justovich <soulden1337@yandex.ru>
2020-05-22 19:25:49 +0500

    little fix

--- LTDCtest/MDK-ARM/LTDCtest/ili9341.d
+++
@@ -1,4 +0,0 @@
-ltdctest\ili9341.o: ..\Drivers\BSP\Components\ili9341\ili9341.c
-ltdctest\ili9341.o: ..\Drivers\BSP\Components\ili9341\ili9341.h
-ltdctest\ili9341.o: ..\Drivers\BSP\Components\ili9341\../Common/lcd.h
-ltdctest\ili9341.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
